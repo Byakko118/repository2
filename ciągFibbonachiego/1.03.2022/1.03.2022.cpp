bf5281755e2e4175de599774e53b9c6ad15cc7b8
ogiusek <100357263+ogiusek@users.noreply.github.com>
2022-03-01 12:50:45 +0100

    Add files via upload

---
+++ ciągFibbonachiego/1.03.2022/1.03.2022.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int Fibbonachi(int repeat){
+	int firstNumber = 0;
+	int secondNumber = 1;
+	for (int i = 0; i  < repeat; i ++){
+		if (firstNumber > secondNumber) {
+			secondNumber += firstNumber;
+			cout << secondNumber;
+		} else {
+			firstNumber += secondNumber;
+			cout << firstNumber;
+		}
+		cout << endl;
+	}
+	return 0;
+}
+int main(){
+	cout << "ile chcesz liczb fibbonachiego : ";
+	int number;
+	cin >> number;
+	Fibbonachi(number);
+}
